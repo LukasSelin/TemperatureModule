04766aa29f36347ddde748a76428be9c05d5f47a
Lukas Selin <lukas.selin@viskan.se>
2020-09-09 10:41:37 +0200

    Updated css

---
+++ Temperature_Webmodule/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperatureModule.Webpage.Models
+{
+    public enum PTIndex
+    {
+        Air = 0,
+        Water = 1
+    }
+}
--- Temperature_Webmodule/Models/LatestTemperatureUnit.cs
+++ Temperature_Webmodule/Models/LatestTemperatureUnit.cs
@@ -8,12 +8,35 @@ namespace TemperatureModule.Webpage.Models
 {
     public class LatestTemperatureUnit
     {
+        public LatestTemperatureUnit(IEnumerable<UnitData> unitData)
+        {
+            Latest = unitData.Last();
+            Highest = GetHighestRecord(unitData);
+            Lowest = GetLowestRecord(unitData);
+
+            Average = Math.Round(unitData.Average(e => e.DataPTValue), 1);
+        }
+
         public UnitData Latest { get; set; }
         public UnitData Highest { get; set; }
         public UnitData Lowest { get; set; }
 
         public double Average { get; set; }
-        public int NumberOfRecords { get; set; }
 
+        private UnitData GetHighestRecord(IEnumerable<UnitData> units)
+        {
+            var data = units.Max(i => i.DataPTValue);
+            var unit = units.Where(i => i.DataPTValue == data).ToArray();
+
+            return unit[0];
+        }
+
+        private UnitData GetLowestRecord(IEnumerable<UnitData> units)
+        {
+            var data = units.Min(i => i.DataPTValue);
+            var unit = units.Where(i => i.DataPTValue == data).ToArray();
+
+            return unit[0];
+        }
     }
 }
